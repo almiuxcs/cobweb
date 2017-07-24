a6cbe7fd1a6173b87c2bac08cd7154ec84e19e98
Marcello <marcello_1000@yahoo.com.br>
2017-07-23 23:58:54 -0300

    plot ax(1-x)

--- cobwebplot.py
+++ cobwebplot.py
@@ -1,10 +1,15 @@
 from matplotlib import pyplot as plt 
-
+import numpy as np
 
 def plot(a, x0=0.1):	
 
-	plt.plot([0, 1], [0, 1])
+	x = np.linspace(0, 1, 1000)
+	y = 2 * x * (1 - x)
+
+	plt.plot(x, y, color="black")
+	plt.plot([0, 1], [0, 1], color="red")
 	plt.show()
+
 
 if __name__ == "__main__":
 	plot(2)
