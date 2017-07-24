22e7d3ed7c34e01e6fdf0ced0d7567fb2577f5a6
Marcello <marcello_1000@yahoo.com.br>
2017-07-24 01:17:00 -0300

    Changed axis to 1.2

--- cobwebplot.py
+++ cobwebplot.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt 
 import numpy as np
 
-def plot(a, x0=0.1):
+def plot(a, x0=0.1, iterations=50):
 	""" Plot a cobweb graph using the equation:
 		a * x * (1 - x)
 	
@@ -14,17 +14,19 @@ def plot(a, x0=0.1):
 	# If x0 isn't valid 
 	if x0 <= 0 or x0 >= 1: return
 
+	plt.title("Cobweb Plot")
+
 	# Plot f(x) = ax(1 - x)
 	x = np.linspace(0, 1, 1000)
 	fx = a * x * (1 - x)	
-	plt.plot(x, fx, color="black")
+	plt.plot(x, fx, color="black", label="f(x) = ax(1-x)")
 	
 	# Plot y = x
-	plt.plot([0, 1], [0, 1], color="red")
+	plt.plot([0, 1.2], [0, 1.2], color="red")
 
 	# Plot f^50(x0)
 	last_x, last_y = x0, 0
-	for _ in range(50):
+	for _ in range(iterations):
 		next_x = a * last_x * (1 - last_x)
 		# Plot vertical line 
 		plt.plot([last_x, last_x], [last_y, next_x], color="black")
@@ -37,7 +39,7 @@ def plot(a, x0=0.1):
 
 
 def main():
-	plot(3.3, 0.9)
+	plot(2)
 	
 
 if __name__ == "__main__":
