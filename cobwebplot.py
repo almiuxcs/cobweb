cb9b620730013e79723028f426a440d5a7aa17fc
Marcello <marcello_1000@yahoo.com.br>
2017-07-24 00:34:01 -0300

    Plot x0

--- cobwebplot.py
+++ cobwebplot.py
@@ -1,15 +1,27 @@
 from matplotlib import pyplot as plt 
 import numpy as np
 
-def plot(a, x0=0.1):	
+def plot(a, x0=0.1):
 
+	# Plot f(x) = ax(1 - x)
 	x = np.linspace(0, 1, 1000)
-	y = 2 * x * (1 - x)
-
-	plt.plot(x, y, color="black")
+	fx = a * x * (1 - x)	
+	plt.plot(x, fx, color="black")
+	
+	# Plot y = x
 	plt.plot([0, 1], [0, 1], color="red")
+
+	# Plot f^50(x0)
+	next_x = a * x0 * (1 - x0)
+	plt.plot([x0, x0], [0, next_x], color="black")
+	plt.plot([x0, next_x], [next_x, next_x], color="red")
+
 	plt.show()
 
 
-if __name__ == "__main__":
+def main():
 	plot(2)
+	
+
+if __name__ == "__main__":
+	main()
