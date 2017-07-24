d0ad402c8075635a7c94b9b2485a98a95f7b8718
Marcello <marcello_1000@yahoo.com.br>
2017-07-24 00:41:45 -0300

    Works for a=2

--- cobwebplot.py
+++ cobwebplot.py
@@ -2,6 +2,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 def plot(a, x0=0.1):
+	""" Plot a cobweb graph using the equation:
+		a * x * (1 - x)
+	
+		It does 50 iterations to plot the graph and 
+		the default value of x0 is 0.1
+	"""
 
 	# Plot f(x) = ax(1 - x)
 	x = np.linspace(0, 1, 1000)
@@ -12,9 +18,15 @@ def plot(a, x0=0.1):
 	plt.plot([0, 1], [0, 1], color="red")
 
 	# Plot f^50(x0)
-	next_x = a * x0 * (1 - x0)
-	plt.plot([x0, x0], [0, next_x], color="black")
-	plt.plot([x0, next_x], [next_x, next_x], color="red")
+	last_x, last_y = x0, 0
+	for _ in range(50):
+		next_x = a * last_x * (1 - last_x)
+		# Plot vertical line 
+		plt.plot([last_x, last_x], [last_y, next_x], color="black")
+		# Plot horizontal line
+		plt.plot([last_x, next_x], [next_x, next_x], color="red")
+
+		last_x, last_y = next_x, next_x
 
 	plt.show()
 
